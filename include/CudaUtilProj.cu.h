89af724a4e45d037fea965ad998f0675cf3a8e93
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2014-11-02 19:49:22 +0100

    Så, nu virker det bedre

--- include/CudaUtilProj.cu.h
+++ include/CudaUtilProj.cu.h
@@ -527,7 +527,7 @@ void transpose3d( REAL*             inp_d,
    int  dimy = ceil( ((REAL) width)/tile );
    int  dimz = ceil( ((REAL) depth)/tile );
    dim3 block(tile, tile, tile);
-   dim3 grid (dimz, dimx, dimy);
+   dim3 grid (dimz, dimy, dimx);
    //dim3 grid (dimx, dimy, dimz);
 
    //2. execute the kernel
